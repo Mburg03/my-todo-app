cad4a6593553da6f33ee52a9723d9c0a5e336c54
Mario Umaña <00063121@uca.edu.sv>
2023-09-05 11:20:31 -0600

    Changes on GUI

--- gui.py
+++ gui.py
@@ -9,10 +9,9 @@ add_button = sg.Button("  Add  ", key="add_button")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=[45,10])
 edit_button = sg.Button("  Edit  ",key="edit_button")
 
-
 window = sg.Window('My To-Do App',
                    layout=[
-                       [label],
+                           [label],
                            [input_box, add_button],
                            [label2],
                            [edit_input],
@@ -29,27 +28,27 @@ while True:
     match event:
         case 'todos':
             window['todo_to_edit'].update(value=values['todos'][0])
+            
         case "add_button":
             todos = functions.get_todos()
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
-
             functions.write_todos(todos)
             print(f"Todo: {new_todo}Status: Added succesfully!")
+            
             window['todos'].update(values=todos)
             window['todo'].update(value="")
             
         case "edit_button": 
             todo_to_edit = values['todos'][0]
             new_todo_to_edit = values['todo_to_edit']
-            
             todos = functions.get_todos()       
             todo_index = todos.index(todo_to_edit)     
             todos[todo_index] = new_todo_to_edit + "\n"
+            
             functions.write_todos(todos)
             window['todos'].update(values=todos)
             window['todo_to_edit'].update(value="")
-       
             
         case sg.WIN_CLOSED:
             break

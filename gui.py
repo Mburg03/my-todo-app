5cf8f2d618739214463204ec38f5c7474d1b3b66
Mario Umaña <00063121@uca.edu.sv>
2023-09-05 23:55:23 -0600

    Add: Complete todo and exit program buttons

--- gui.py
+++ gui.py
@@ -8,6 +8,8 @@ edit_input = sg.InputText(tooltip="New edited todo", key="todo_to_edit")
 add_button = sg.Button("  Add  ", key="add_button")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=[45,10])
 edit_button = sg.Button("  Edit  ",key="edit_button")
+complete_button = sg.Button("Complete", key="complete_button")
+exit_button = sg.Button("Exit", key="exit_button")
 
 window = sg.Window('My To-Do App',
                    layout=[
@@ -15,7 +17,8 @@ window = sg.Window('My To-Do App',
                            [input_box, add_button],
                            [label2],
                            [edit_input],
-                           [list_box, edit_button]
+                           [list_box, edit_button, complete_button],
+                           [exit_button]
                            ],
                    font=('Helvetica', 10)
                    )
@@ -50,6 +53,18 @@ while True:
             window['todos'].update(values=todos)
             window['todo_to_edit'].update(value="")
             
+        case "complete_button":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
+            
+        case "exit_button":
+            break
+                
         case sg.WIN_CLOSED:
             break
 
